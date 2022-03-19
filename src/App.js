b5af3361d1a59f77f4fc91a92bee4f6a6ec7e85f
HB7723 <hb772325@gmail.com>
2022-03-20 00:42:06 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import HeroSection from "./Components/HeroSection/HeroSection";
 
-import SpeakerSection from "./Components/SpeakerSection/SpeakerSection";
+//import SpeakerSection from "./Components/SpeakerSection/SpeakerSection";
 import SponsorSection from "./Components/SponsorSection/SponsorSection";
 
 import About from "./Components/About/About";
 import PrizePool from "./Components/PrizePool/PrizePool";
 import Register from "./Components/Register/Register";
+
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -17,10 +19,11 @@ function App() {
       <Header />
       <HeroSection />
       <About />
-      <SpeakerSection />
+      {/* <SpeakerSection /> */}
       <PrizePool />
       <SponsorSection />
       <Register />
+      <Footer />
     </div>
   );
 }
